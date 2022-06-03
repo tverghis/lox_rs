a9aa3587e0acb2dacbeb886a7ce9576504bed4f2
Tarun Verghis <tarun.verghis@gmail.com>
2022-06-02 22:10:05 -0700

    Add TokenKind

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,10 +1,64 @@
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct Token {
     span: Span,
+    kind: TokenKind,
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 struct Span {
     start: usize,
     end: usize,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum TokenKind {
+    // Single-character tokens
+    LParen,
+    RParen,
+    LBrace,
+    RBrace,
+    Comma,
+    Dot,
+    Minus,
+    Plus,
+    Semicolon,
+    Slash,
+    Asterisk,
+
+    // Single- or double-character tokens
+    Exclamation,
+    ExclamationEqual,
+    Equal,
+    EqualEqual,
+    GreaterThan,
+    GreaterThanEqual,
+    LessThan,
+    LessThanEqual,
+
+    // Literals
+    Identifier(String),
+    QuotedString(String),
+    Number(f64),
+
+    // Keywords
+    And,
+    Class,
+    Else,
+    False,
+    Fun,
+    For,
+    If,
+    Nil,
+    Or,
+    Print,
+    Return,
+    Super,
+    This,
+    True,
+    Var,
+    While,
+
+    Eof,
+
+    Unknown,
 }
